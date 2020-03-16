ed5b44a2c9c8dff02bcb49f351a841db717bdd73
Joshua Burke <burke.joshua.james@gmail.com>
2020-03-16 16:47:07 -0400

    Add starter files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      textInput: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({
+      textInput: evt.target.value
+    });
+  }
+
+  render() {
+    return <TextInput input={this.state.textInput} />;
+  }
+}
+
+function TextInput(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Type in the textarea below, and have the text appear in the Div at the
+        bottom
+      </h1>
+      <textarea className="source" />
+      <div className="target">
+        <p>{props.input}</p>
+      </div>
     </div>
   );
 }
-
-export default App;
